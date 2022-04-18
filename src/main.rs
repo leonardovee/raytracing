9cdc2c427cdc6ddc25c5589d701d32907a026321
leonardovee <leoveeph@gmail.com>
2022-04-17 21:31:55 -0300

    refactor: simplifying the ray-sphere intersection logic

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::{fs::File, io::Write};
 use rendering::{render_pixel, Ray};
 use vector::Vector3;
 
-mod vector;
 mod rendering;
+mod vector;
 
 fn main() {
     // image definitions
@@ -17,10 +17,29 @@ fn main() {
     let viewport_width = aspect_ratio * viewport_height;
     let focal_length = 1.0;
 
-    let origin = Vector3 { x: 0.0, y: 0.0, z: 0.0 };
-    let horizontal = Vector3 { x: viewport_width, y: 0.0, z: 0.0 };
-    let vertical = Vector3 { x: 0.0, y: viewport_height, z: 0.0 };
-    let lower_left_corner = origin - (horizontal / 2.0) - (vertical / 2.0) - Vector3 { x: 0.0, y: 0.0, z: focal_length };
+    let origin = Vector3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+    let horizontal = Vector3 {
+        x: viewport_width,
+        y: 0.0,
+        z: 0.0,
+    };
+    let vertical = Vector3 {
+        x: 0.0,
+        y: viewport_height,
+        z: 0.0,
+    };
+    let lower_left_corner = origin
+        - (horizontal / 2.0)
+        - (vertical / 2.0)
+        - Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: focal_length,
+        };
 
     // render process
     let mut image_buffer: Vec<String> = vec![
@@ -28,7 +47,7 @@ fn main() {
         image_width.to_string(),
         String::from(" "),
         image_height.to_string(),
-        String::from("\n255\n")
+        String::from("\n255\n"),
     ];
 
     for j in (0..image_height as i64).rev() {
@@ -36,7 +55,10 @@ fn main() {
             let u = i as f64 / (image_width - 1) as f64;
             let v = j as f64 / (image_height - 1.0);
 
-            let ray = Ray { origin, direction: lower_left_corner + (horizontal * u) + (vertical * v) - origin};
+            let ray = Ray {
+                origin,
+                direction: lower_left_corner + (horizontal * u) + (vertical * v) - origin,
+            };
             let pixel_color = ray.color();
 
             render_pixel(&mut image_buffer, pixel_color);
